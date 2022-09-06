bebe9ab929f5d26299edfc7f9748ee05dd07590a
Joey <erjo.guce214@gmail.com>
2022-09-06 15:22:54 -0400

    Add SidebarContent component, Refactor Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,33 @@
 import React from "react"
 
+function SidebarContent() {
+    <li>
+        <img src=""></img>
+        <p>{}</p>
+    </li>
+}
+
+
 function Sidebar() {
-    
+    <div className="sidebar">
+        <div className="sidebar-logo">
+            <img src=""></img>
+            <h1>RealliChili</h1>
+        </div>
+        <div className="sidebar-content">
+            <ul>
+                <SidebarContent />
+                <SidebarContent />
+                <SidebarContent />
+                <SidebarContent />
+                <SidebarContent />
+            </ul>
+        </div>
+        <div className="sidebar-logout">
+            <img src=""></img>
+            <button></button>
+        </div>
+    </div>
 }
 
 export default Sidebar;
